ae5ece2967b10316528ac4b7e78aea91cbd58ad5
Sam_76 <muhammadusamah76@gmail.com>
2024-11-28 20:36:41 +0700

    Banner setup

--- src/Component/Banner/Banner.js
+++ src/Component/Banner/Banner.js
@@ -1,7 +1,14 @@
+import style from './Banner.module.css'
+
 const Banner = () => {
     return (
-        <div>
-            <h1>Hello World!!</h1>
+        <div className={style.container_fluid}>
+            <div className={style.banner_info}>
+                <h1>Banner Info</h1>
+            </div>
+            <div className={style.banner_background}>
+                <img src='https://picsum.photos/id/36/2000/480' className={style.image_banner} />
+            </div>
         </div>
     )
 }
