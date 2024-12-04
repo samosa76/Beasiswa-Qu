ffb92046ec0c2cc8643b16ba2b7d3f8be153c7e0
Sam_76 <muhammadusamah76@gmail.com>
2024-12-04 22:29:27 +0700

    responsive logo nav & program image

--- src/Component/Program/Program.js
+++ src/Component/Program/Program.js
@@ -4,16 +4,11 @@ import ProgramItem from "./ProgramItem/ProgramItem";
 const Program = () => {
     return (
         <div className={`${style.container}`}>
-            <div className={style.program_title}>
-                <h1>Our Programs</h1>
-            </div>
             <div className={style.program_content}>
-                <div className={style.content_image}>
-                    <img src="https://picsum.photos/400" alt="program-image" />
+                <div className={style.program_image}>
+                    <img src="asset/background/program_image.png" alt="program-image" />
                 </div>
-
-                <div className={style.content_programs}>
-                    
+                <div className={style.programs}>
                     <ProgramItem title="Beasiswa Al-Qur'an"/>
                     <ProgramItem title="Beasiswa Kader Bangsa"/>
                     <ProgramItem title="Beasiswa Prestasi"/>
