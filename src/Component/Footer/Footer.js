543e3973124805b6095eecd5d900cd5dbfdd8d39
Sam_76 <muhammadusamah76@gmail.com>
2024-12-05 22:25:28 +0700

    Footer Finished

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -3,7 +3,37 @@ import style from "./Footer.module.css"
 const Footer = () => {
     return (
         <div className={style.footer_container}>
-            <h1>Footer</h1>
+            <div className={style.footer_logo}>
+                <img src="Asset/icon/footer_logo.png" alt="footer-logo" />
+            </div>
+
+            <div className={style.footer_contact_us}>
+                <div className={style.contact_us_title}>
+                    <h4>Contact Us</h4>
+                </div>
+                <div className={style.contact_us_items}>
+                    <div className={style.contact_us_item}>
+                        <img src="Asset/icon/icon_mark.png" alt="icon-mark"/>
+                        <p>Villa Pedurenan Indah 1 Blok C No. 6 Padurenan Mustika Jaya, Bekasi, Jawa  Barat, Indonesia</p>
+                    </div>
+
+                    <div className={style.contact_us_item}>
+                        <img src="Asset/icon/icon_phone.png" alt="icon-phone"/>
+                        <p>0859-3517-4648</p>
+                    </div>
+
+                    <div className={style.contact_us_item}>
+                        <img src="Asset/icon/icon_mail.png" alt="icon-mail"/>
+                        <p>qubeasiswa@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className={style.contact_us_sosmed}>
+                <img src="asset/icon/icon_ig.png" alt="icon-instagram"/>
+                <img src="asset/icon/icon_fb.png" alt="icon-facbook"/>
+                <img src="asset/icon/icon_tiktok.png" alt="icon-facebook"/>
+            </div>
         </div>
     );
 }
